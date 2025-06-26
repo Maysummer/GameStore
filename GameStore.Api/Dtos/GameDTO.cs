0d4f13f786a017c10b5909f6612f33eba482f21c
Udojike <nudojike@qore.inc>
2025-06-26 13:00:53 +0100

    Add project files.

---
+++ GameStore.Api/Dtos/GameDTO.cs
@@ -0,0 +1,3 @@
+namespace GameStore.Api.DTOs;
+
+public record class GameDTO(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,21 @@
+
+using GameStore.Api.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+List<GameDTO> games = [
+    new (1, "Street Fighter II", "Fighting", 19.9M, new DateOnly(1992, 7, 15)),
+    new (2, "Final Fantasy XIV", "Roleplaying", 59.9M, new DateOnly(2010, 9, 30)),
+    new (3, "FIFA 23", "Sports", 69.99M, new DateOnly(2022, 9, 27))
+];
+
+var app = builder.Build();
+
+//GET /games
+app.MapGet("games", () => games);
+
+//GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
