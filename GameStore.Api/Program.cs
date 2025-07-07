3f30e4ccd1072bd39098512537a2a2a0c802be77
Udojike <nudojike@qore.inc>
2025-07-07 01:54:34 +0100

    return not found for get if id does not exist

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -17,7 +17,11 @@ var app = builder.Build();
 app.MapGet("games", () => games);
 
 // GET /games/1
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+app.MapGet("games/{id}", (int id) => 
+{
+    GameDTO? game = games.Find(game => game.Id == id);
+    return game is null ? Results.NotFound() : Results.Ok(game);
+}).WithName(GetGameEndpointName);
 
 // POST /games
 app.MapPost("games", (CreateGameDTO newGame) =>
